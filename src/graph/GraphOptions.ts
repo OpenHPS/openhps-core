5d11d9072230cc5b0fb1295fcb4ea52e96010e25
Maxim Van de Wynckel <maxim@mvdw-software.com>
2020-01-18 11:47:57 +0100

    feat: added serialization for graph options

--- src/graph/GraphOptions.ts
+++ src/graph/GraphOptions.ts
@@ -1,3 +1,17 @@
+import 'reflect-metadata';
+import { jsonObject, TypedJSON } from 'typedjson';
+
+@jsonObject
 export class GraphOptions {
+
+    public serialize(): string {
+        const serializer = new TypedJSON(Object.getPrototypeOf(this).constructor);
+        return serializer.stringify(this);
+    }
+
+    public static deserialize<T extends GraphOptions>(serialized: string, dataType: new () => T): T {
+        const serializer = new TypedJSON(dataType);
+        return serializer.parse(serialized);
+    }
 
 }
--- src/graph/GraphPushOptions.ts
+++ src/graph/GraphPushOptions.ts
@@ -1,5 +1,8 @@
+import 'reflect-metadata';
 import { GraphOptions } from "./GraphOptions";
+import { jsonObject } from 'typedjson';
 
+@jsonObject
 export class GraphPushOptions extends GraphOptions {
     
 }
