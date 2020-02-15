fa7e94fe7577ca8ba8a483196a505a39973760f9
Maxim Van de Wynckel <maxim@mvdw-software.com>
2020-02-15 15:04:53 +0100

    chore: added triangulation

--- src/nodes/processing/TriangulationNode.ts
+++ src/nodes/processing/TriangulationNode.ts
@@ -1,4 +1,4 @@
-import { SensorObject, DataObject, DataFrame } from "../../data";
+import { SensorObject, DataObject, DataFrame, RelativeAngleLocation } from "../../data";
 import { ObjectProcessingNode } from "../ObjectProcessingNode";
 import { Model } from "../../Model";
 
@@ -17,7 +17,49 @@ export class TriangulationNode<InOut extends DataFrame> extends ObjectProcessing
 
     public processObject(dataObject: SensorObject): Promise<DataObject> {
         return new Promise((resolve, reject) => {
-          
+            const referencePromises = new Array();
+            const index = new Map<string, RelativeAngleLocation[]>();
+            for (const relativeLocation of dataObject.relativeLocations) {
+                // Only use relative angle locations
+                if (relativeLocation instanceof RelativeAngleLocation) {
+                    if (index.has(relativeLocation.referenceObjectUID)) {
+                        index.get(relativeLocation.referenceObjectUID).push(relativeLocation);
+                    } else {
+                        index.set(relativeLocation.referenceObjectUID, [relativeLocation]);
+                    }
+                    referencePromises.push(this._findObjectByName(relativeLocation.referenceObjectUID, relativeLocation.referenceObjectType));
+                }
+            }
+
+            Promise.all(referencePromises).then(referenceObjects => {
+                // Filter relative locations that have an absolute location
+                const filteredRelativeLocations = new Array<RelativeAngleLocation>();
+                const objectCache = new Map<string, DataObject>();
+                referenceObjects.forEach((referenceObject: DataObject) => {
+                    if (referenceObject.absoluteLocation !== undefined) {
+                        objectCache.set(referenceObject.uid, referenceObject);
+                        index.get(referenceObject.uid).forEach(relativeLocation => {
+                            filteredRelativeLocations.push(relativeLocation);
+                        });
+                    }
+                });
+
+                const objects = new Array<DataObject>();
+                const points = new Array();
+                const angles = new Array();
+                filteredRelativeLocations.forEach(filteredRelativeLocation => {
+                    const object = objectCache.get(filteredRelativeLocation.referenceObjectUID);
+                    objects.push(object);
+                    points.push(object.absoluteLocation);
+                    angles.push(filteredRelativeLocation.angle);
+                });
+
+                switch (filteredRelativeLocations.length) {
+                    
+                }
+            }).catch(ex => {
+                reject(ex);
+            }); 
         });
     }
 
