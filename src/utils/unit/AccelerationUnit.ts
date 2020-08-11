1b9d8243a963fd35982efff510a8df6d0956cc66
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2020-08-11 11:02:01 +0200

    chore: code cleanup (coverage and maintainability)

--- src/utils/unit/AccelerationUnit.ts
+++ src/utils/unit/AccelerationUnit.ts
@@ -4,9 +4,17 @@ import { Unit } from "./Unit";
 @SerializableObject()
 export class AccelerationUnit extends Unit {
 
-    public static readonly METER_PER_SECOND_SQUARE = new Unit("meter per second squared", {
+    public static readonly METER_PER_SECOND_SQUARE = new AccelerationUnit("meter per second squared", {
         baseName: "acceleration",
-        aliases: ["m/s^2", "meters per second squared"]
+        aliases: ["m/s^2", "m/s2", "meters per second squared"]
+    });
+
+    public static readonly GRAVITATIONAL_FORCE = new AccelerationUnit("gravitational force", {
+        baseName: "acceleration",
+        aliases: ["g-force", 'G', 'GS'],
+        definitions: [
+            { magnitude: 9.78033, unit: "m/s^2" }
+        ]
     });
 
 }
--- src/utils/unit/AngularVelocityUnit.ts
+++ src/utils/unit/AngularVelocityUnit.ts
@@ -4,11 +4,11 @@ import { Unit } from "./Unit";
 @SerializableObject()
 export class AngularVelocityUnit extends Unit {
 
-    public static readonly RADIAN_PER_SECOND = new Unit("radian per second", {
+    public static readonly RADIAN_PER_SECOND = new AngularVelocityUnit("radian per second", {
         baseName: "angularvelocity",
         aliases: ["rad/s", "radians per second"]
     });
-    public static readonly DEGREE_PER_SECOND = new Unit("degree per second", {
+    public static readonly DEGREE_PER_SECOND = new AngularVelocityUnit("degree per second", {
         baseName: "angularvelocity",
         aliases: ["deg/s", "degrees per second"],
         definitions: [
