d91f204666b670394dea5def9aa6d24ff99ca1d3
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2020-12-16 16:19:10 +0100

    fix: frame merge node should merge grouped frames

--- src/nodes/shapes/ObjectMergeNode.ts
+++ src/nodes/shapes/ObjectMergeNode.ts
@@ -15,56 +15,53 @@ export class ObjectMergeNode<InOut extends DataFrame> extends FrameMergeNode<InO
         );
     }
 
-    public merge(frames: InOut[], objectUID: string): Promise<InOut> {
-        return new Promise<InOut>((resolve) => {
-            const mergedFrame = frames[0];
-            const mergedObjects: Map<string, DataObject[]> = new Map();
-            mergedFrame.getObjects().forEach((object) => {
-                if (mergedObjects.get(object.uid)) {
-                    mergedObjects.get(object.uid).push(object);
-                } else {
-                    mergedObjects.set(object.uid, [object]);
-                }
-            });
-
-            const existingObject = mergedFrame.getObjectByUID(objectUID);
-
-            for (let i = 1; i < frames.length; i++) {
-                const frame = frames[i];
-                const object = frame.getObjectByUID(objectUID);
-
-                if (object) {
-                    // Merge object
-                    object.relativePositions.forEach((value) => {
-                        existingObject.addRelativePosition(value);
-                    });
-                    frame.getObjects().forEach((object) => {
-                        if (!mergedFrame.hasObject(object)) {
-                            // Add object
-                            mergedFrame.addObject(object);
-                            mergedObjects.set(object.uid, [object]);
-                        } else {
-                            mergedObjects.get(object.uid).push(object);
-                        }
-                    });
-
-                    // Merge properties
-                    Object.keys(frame).forEach((propertyName) => {
-                        const value = (mergedFrame as any)[propertyName];
-                        if (value === undefined || value === null) {
-                            (mergedFrame as any)[propertyName] = (frame as any)[propertyName];
-                        }
-                    });
-                }
+    public merge(frames: InOut[], objectUID: string): InOut {
+        const mergedFrame = frames[0];
+        const mergedObjects: Map<string, DataObject[]> = new Map();
+        mergedFrame.getObjects().forEach((object) => {
+            if (mergedObjects.get(object.uid)) {
+                mergedObjects.get(object.uid).push(object);
+            } else {
+                mergedObjects.set(object.uid, [object]);
             }
-
-            // Merge objects using the merging function
-            mergedObjects.forEach((values) => {
-                const mergedObject = this.mergeObjects(values);
-                mergedFrame.addObject(mergedObject);
-            });
-
-            resolve(mergedFrame);
         });
+
+        const existingObject = mergedFrame.getObjectByUID(objectUID);
+
+        for (let i = 1; i < frames.length; i++) {
+            const frame = frames[i];
+            const object = frame.getObjectByUID(objectUID);
+
+            if (object) {
+                // Merge object
+                object.relativePositions.forEach((value) => {
+                    existingObject.addRelativePosition(value);
+                });
+                frame.getObjects().forEach((object) => {
+                    if (!mergedFrame.hasObject(object)) {
+                        // Add object
+                        mergedFrame.addObject(object);
+                        mergedObjects.set(object.uid, [object]);
+                    } else {
+                        mergedObjects.get(object.uid).push(object);
+                    }
+                });
+
+                // Merge properties
+                Object.keys(frame).forEach((propertyName) => {
+                    const value = (mergedFrame as any)[propertyName];
+                    if (value === undefined || value === null) {
+                        (mergedFrame as any)[propertyName] = (frame as any)[propertyName];
+                    }
+                });
+            }
+        }
+
+        // Merge objects using the merging function
+        mergedObjects.forEach((values) => {
+            const mergedObject = this.mergeObjects(values);
+            mergedFrame.addObject(mergedObject);
+        });
+        return mergedFrame;
     }
 }
