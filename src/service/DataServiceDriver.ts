30fac72bb6c6a64b28b7d21b7b2b90bf609c0f11
Maxim Van de Wynckel <maxim@mvdw-software.com>
2021-03-15 19:08:49 +0100

    feat: added serializer and deserializer as a default part of a data service driver

--- src/service/DataServiceDriver.ts
+++ src/service/DataServiceDriver.ts
@@ -1,5 +1,6 @@
 import { Service } from './Service';
 import { FilterQuery } from './FilterQuery';
+import { DataSerializer } from '../data';
 
 /**
  * DataService driver for storing and querying data objects
@@ -7,9 +8,17 @@ import { FilterQuery } from './FilterQuery';
  */
 export abstract class DataServiceDriver<I, T> extends Service {
     public dataType: new () => T;
+    protected serialize: (obj: T) => any;
+    protected deserialize: (obj: any) => T;
 
-    constructor(dataType: new () => T) {
+    constructor(
+        dataType: new () => T,
+        serializer: (obj: T) => any = (obj) => DataSerializer.serialize(obj),
+        deserializer: (obj: any) => T = (obj) => DataSerializer.deserialize(obj),
+    ) {
         super();
+        this.serialize = serializer;
+        this.deserialize = deserializer;
 
         if (dataType) {
             this.name = dataType.name;
