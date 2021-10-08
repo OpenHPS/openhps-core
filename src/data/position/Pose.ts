d6ea3b0107c75268f1ddea8ae7aa35621f8c0b2a
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2021-10-08 19:03:02 +0200

    fix: add orientation to pose output to position

--- src/data/position/Pose.ts
+++ src/data/position/Pose.ts
@@ -4,32 +4,32 @@ import { Matrix4 } from '../../utils/math/';
 import { SerializableMember, SerializableObject } from '../decorators';
 import { Absolute3DPosition } from './Absolute3DPosition';
 import { Orientation } from './Orientation';
+import { Position } from './Position';
 
 /**
  * Position and orientation
  */
 @SerializableObject()
-export class Pose extends Matrix4 {
+export class Pose extends Matrix4 implements Position {
     /**
      * Position recording timestamp
      */
     @SerializableMember({
         index: true,
     })
-    positionTimestamp: number = TimeService.now();
-    /**
-     * Position recording timestamp
-     */
-    @SerializableMember({
-        index: true,
-    })
-    orientationTimestamp: number = TimeService.now();
+    timestamp: number = TimeService.now();
     /**
      * Position unit
      */
     @SerializableMember()
     unit: LengthUnit = LengthUnit.METER;
 
+    /**
+     * Get a pose from a 4d matrix
+     *
+     * @param {Matrix4} matrix 4x4 Matrix
+     * @returns {Pose} Pose instance
+     */
     static fromMatrix4<T extends Pose>(matrix: Matrix4): T {
         const pose = new this();
         pose.fromArray(matrix.toArray());
@@ -38,8 +38,7 @@ export class Pose extends Matrix4 {
 
     static fromPosition<T extends Pose>(position: Absolute3DPosition, orientation: Orientation): T {
         const pose = new this();
-        pose.positionTimestamp = position.timestamp;
-        pose.orientationTimestamp = orientation.timestamp;
+        pose.timestamp = position.timestamp;
         pose.unit = position.unit;
         const vector = position.toVector3();
         pose.makeRotationFromQuaternion(orientation);
@@ -50,7 +49,7 @@ export class Pose extends Matrix4 {
     get orientation(): Orientation {
         const rotationMatrix = this.extractRotation(this);
         const orientation = Orientation.fromRotationMatrix(rotationMatrix) as Orientation;
-        orientation.timestamp = this.orientationTimestamp;
+        orientation.timestamp = this.timestamp;
         return orientation;
     }
 
@@ -61,8 +60,9 @@ export class Pose extends Matrix4 {
             positionMatrix.elements[13],
             positionMatrix.elements[14],
         );
-        position.timestamp = this.positionTimestamp;
+        position.timestamp = this.timestamp;
         position.unit = this.unit;
+        position.orientation = this.orientation;
         return position;
     }
 }
