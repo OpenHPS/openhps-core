83ccb2e516d98dca239c7f1f89c70a4cf22ab4db
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-09-18 17:57:34 +0200

    test: sensor object mock objects

--- test/mock/data/object/AccelerometerObject.ts
+++ test/mock/data/object/AccelerometerObject.ts
@@ -2,5 +2,5 @@ import { SensorObject, SerializableObject, Vector3 } from "../../../../src";
 
 @SerializableObject()
 export class AccelerometerObject extends SensorObject<Vector3> {
-    raw = new Vector3()
+    value = new Vector3()
 }
