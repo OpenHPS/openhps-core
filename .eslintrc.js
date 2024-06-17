5f2a2f7c51132bcce5dd560061f9f5bb7718e2f1
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2024-06-17 16:20:56 +0200

    chore: linting issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
         "import/no-cycle": ["error", { "maxDepth": 15 }],
         "import/no-unresolved": "off",
         "prettier/prettier": ["error"],
-        "jsdoc/check-tag-names": ["error", { "definedTags": ["category"] }],
+        "jsdoc/check-tag-names": ["error", { "definedTags": ["category", "rdf"] }],
     },
     "extends": [
         "eslint:recommended",
